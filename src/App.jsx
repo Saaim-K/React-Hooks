e61a3e7e02743b3f80ef73ed770451b932a6094c
Saaim-K <saaimkhan221@gmail.com>
2023-01-14 22:04:30 +0500

    React Hooks

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import EffectTutorial from "./UseEffect/EffectTutorial";
+import ReducerTutorial from "./UseReducer/ReducerTutorial";
+import StateTutorial from "./UseState/StateTutorial";
+import ContextTutorial from "./UseContext/ContextTutorial";
+import ImperativeHandle from "./UseImperativeHandle/ImperativeHandle";
+import RefTutorial from "./UseRef/RefTutorial";
+import MemoTutorial from "./UseMemo/MemoTutorial";
+import CallBackTutorial from "./UseCallback/CallbackTutorial";
+
+const App = () => {
+  return (
+    <div>
+      {/* <EffectTutorial /> */}
+      {/* <ReducerTutorial /> */}
+      {/* <StateTutorial /> */}
+      {/* <ContextTutorial /> */}
+      {/* <ImperativeHandle /> */}
+      {/* <RefTutorial /> */}
+      {/* <MemoTutorial /> */}
+      {/* <CallBackTutorial /> */}
+    </div >
+  );
+}
+
+export default App;
---
+++ src/UseState/StateTutorial.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const StateTutorial = () => {
+  const [inputValue, setInputValue] = useState("Pedro");
+
+  let onChange = (e) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+  };
+
+  return (
+    <div>
+      <input placeholder="enter something..." onChange={onChange} />
+      <input placeholder="enter something..." onChange={(e) => { setInputValue(e.target.value) }} />
+      {inputValue}
+    </div>
+  );
+};
+
+export default StateTutorial;
